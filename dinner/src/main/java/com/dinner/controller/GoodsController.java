f079ecece699418db7c77230af04d9ac90c70257
qy86wcj <861486800@qq.com>
2018-11-23 09:06:18 +0800

    盘点

---
+++ dinner/src/main/java/com/dinner/controller/GoodsController.java
@@ -0,0 +1,39 @@
+package com.dinner.controller;
+
+import com.dinner.service.GoodsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * className:GoodsController
+ * discriptoin:
+ * author:王朝军
+ * createTime:2018-11-22 20:53
+ */
+@Controller
+public class GoodsController {
+    @Autowired
+    private GoodsService goodsService;
+    @RequestMapping("/toList")
+    public String tologin(Map map){
+/*        List<Map> maps = goodsService.goodsList(map);
+        System.out.println(maps);*/
+        return "after/details";
+    }
+/*    @RequestMapping("/goodsList")
+    @ResponseBody
+    public Object getOrderList(@RequestParam Map map){
+        Map tempMap = new HashMap();
+        //绑定数据  分页数据
+        tempMap.put("code", 0);
+        tempMap.put("msg", "");
+        tempMap.put("data",goodsService.goodsList(map));
+        return tempMap;
+    }*/
+}
