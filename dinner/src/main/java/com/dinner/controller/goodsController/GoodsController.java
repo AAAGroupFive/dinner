79d4500cafac0436b425bafbe6ed841de6616277
qy86wcj <861486800@qq.com>
2018-11-23 15:58:51 +0800

    整理

--- dinner/src/main/java/com/dinner/controller/goodsController/GoodsController.java
+++ dinner/src/main/java/com/dinner/controller/goodsController/GoodsController.java
@@ -1,10 +1,15 @@
 package com.dinner.controller.goodsController;
 
+
 import com.dinner.service.goodsService.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,11 +24,11 @@ public class GoodsController {
     private GoodsService goodsService;
     @RequestMapping("/toList")
     public String tologin(Map map){
-/*        List<Map> maps = goodsService.goodsList(map);
-        System.out.println(maps);*/
+        List<Map> maps = goodsService.goodsList(map);
+        System.out.println(maps);
         return "after/details";
     }
-/*    @RequestMapping("/goodsList")
+    @RequestMapping("/goodsList")
     @ResponseBody
     public Object getOrderList(@RequestParam Map map){
         Map tempMap = new HashMap();
@@ -32,5 +37,5 @@ public class GoodsController {
         tempMap.put("msg", "");
         tempMap.put("data",goodsService.goodsList(map));
         return tempMap;
-    }*/
+    }
 }
