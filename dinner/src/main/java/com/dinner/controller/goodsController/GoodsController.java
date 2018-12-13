094e8f1b07a6230267c4c764d89ff74802b40b0a
QY86XFB <936270749@qq.com>
2018-12-13 15:40:03 +0800

    邢芳彬

--- dinner/src/main/java/com/dinner/controller/goodsController/GoodsController.java
+++ dinner/src/main/java/com/dinner/controller/goodsController/GoodsController.java
@@ -2,8 +2,6 @@ package com.dinner.controller.goodsController;
 
 
 import com.dinner.service.goodsService.GoodsService;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
