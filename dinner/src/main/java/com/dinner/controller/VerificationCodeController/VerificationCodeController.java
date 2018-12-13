a72a016b397a3f5845b063ad0bb7383c50b2f593
J-Hsitsung <2019447337@qq.com>
2018-12-13 19:47:45 +0800

    王玺宗111

--- dinner/src/main/java/com/dinner/controller/VerificationCodeController/VerificationCodeController.java
+++ dinner/src/main/java/com/dinner/controller/VerificationCodeController/VerificationCodeController.java
@@ -11,7 +11,6 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
