2c9079acf5322f4240b5b3742ad43da9ed1a20dd
nicole-pan31 <1161402707@qq.com>
2018-12-13 15:12:15 +0800

    Merge branch 'master' into pan
    # Conflicts:
    #	.gitignore
    #	.idea/libraries/Maven__commons_logging_commons_logging_1_2.xml
    #	.idea/libraries/Maven__javax_servlet_servlet_api_2_5.xml
    #	.idea/libraries/Maven__org_apiguardian_apiguardian_api_1_0_0.xml
    #	.idea/libraries/Maven__org_opentest4j_opentest4j_1_1_1.xml
    #	.idea/libraries/Maven__org_slf4j_slf4j_api_1_7_21.xml
    #	.idea/libraries/Maven__org_springframework_spring_aop_4_2_4_RELEASE.xml
    #	.idea/libraries/Maven__org_springframework_spring_beans_4_2_4_RELEASE.xml
    #	.idea/libraries/Maven__org_springframework_spring_context_4_2_4_RELEASE.xml
    #	.idea/libraries/Maven__org_springframework_spring_core_4_2_4_RELEASE.xml
    #	.idea/libraries/Maven__org_springframework_spring_expression_4_2_4_RELEASE.xml
    #	.idea/libraries/Maven__org_springframework_spring_web_4_2_4_RELEASE.xml
    #	.idea/libraries/Maven__org_springframework_spring_webmvc_4_2_4_RELEASE.xml
    #	shiro-web/shiro-web.iml

---
+++ dinner/src/main/java/com/dinner/service/changeDutyService/changeDutyServiceImpl.java
@@ -0,0 +1,65 @@
+package com.dinner.service.changeDutyService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.dinner.dao.changeDutyDao.changeDutyDao;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * className:changeDutyServiceImpl
+ * discription:
+ * author:panjing
+ * createTime:2018-12-08 13:58
+ */
+@Service
+public class changeDutyServiceImpl implements changeDutyService {
+
+    @Autowired
+    private changeDutyDao changeDutyDao;
+    @Override
+    public Map queryChangeDuty() {
+        Map maps = changeDutyDao.queryChangeDuty();
+
+        maps.put("Money",changeDutyDao.queryMoney());
+        return maps;
+    }
+
+    @Override
+    public  List<Map> queryJemp() {
+        List<Map> maps = changeDutyDao.queryJemp();
+
+        return  maps;
+    }
+
+    @Override
+    public int insert(Map map) {
+        String stime = (String) map.get("stime");
+
+        String etime = (String)  map.get("etime");
+        stime = stime.replace("Z", " UTC");//注意是空格+UTC
+        etime = etime.replace("Z", " UTC");//注意是空格+UTC
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z");//注意格式化的表达式
+        try {
+            Date stime1 = format.parse(stime);
+            Date etime1= format.parse(etime);
+            map.put("stime1",stime1);
+            map.put("etime1",etime1);
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return changeDutyDao.insert(map);
+    }
+
+    @Override
+    public List<Map> queryRecord() {
+        return changeDutyDao.queryRecord();
+    }
+}
