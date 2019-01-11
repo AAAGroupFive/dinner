ef98c4fda66a05866a3bedc210136b3e0429232a
nicole-pan31 <1161402707@qq.com>
2019-01-11 19:38:18 +0800

    Merge branch 'master' of https://github.com/AAAGroupFive/dinner
    # Conflicts:
    #	dinner/src/main/java/com/dinner/controller/vipController/VipController.java
    #	dinner/src/main/java/com/dinner/dao/VipDao/VipDao.java
    #	dinner/src/main/java/com/dinner/service/VipService/VipServiceLmpl.java
    #	dinner/src/main/resources/static/after/vipQuery.html

---
+++ dinner/src/main/java/com/dinner/controller/VipController/VipController.java
@@ -0,0 +1,133 @@
+package com.dinner.controller.VipController;
+
+
+import com.dinner.service.VipService.VipService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * className:VipController
+ * discription:
+ * author:panjing
+ * createTime:2018-11-23 15:45
+ */
+@Controller
+@RequestMapping("/VIP")
+public class VipController {
+    @Autowired
+    private VipService vipService;
+
+    @ResponseBody
+    @RequestMapping("/GetSession")
+    public Map GetSession(Model model, HttpServletRequest request){
+
+        Map temp=new HashMap();
+        HttpSession session = request.getSession();
+        Object name = session.getAttribute("userName");
+       temp.put("Empname",name);
+        return temp;
+    }
+    /**
+     * 去会员查询页面
+     * @return
+     */
+    @RequestMapping("/toVipQuery")
+    public String toVipQuery(){
+        return "after/vipQuery";
+    }
+
+
+    /**
+     * 查询会员列表
+     * @param map
+     * @param request
+     * @return
+     */
+    @RequestMapping("/VipQuery")
+    @ResponseBody
+    public Map VipQuery(@RequestParam Map map, HttpServletRequest request){
+
+        Map temp=new HashMap();
+
+
+        temp.put("code",0);
+        temp.put("msg","");
+        temp.put("count",vipService.VipGetPageCount());
+        temp.put("data",vipService.VipQuery(map));
+        return temp;
+
+    }
+
+    /**
+     * 添加会员
+     * @param map
+     * @return
+     */
+
+    @RequestMapping("/VipAdd")
+    public String  VipAdd(@RequestParam Map map){
+
+        int i = vipService.VipAdd(map);
+
+
+        Map temp=new HashMap();
+
+        if(i>-1){
+            temp.put("issuc",true);
+        }else{
+            temp.put("issuc",false  );
+        }
+
+
+
+
+        return "after/vipQuery";
+
+
+
+    }
+
+    /**
+     * 会员更新
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/VipUpdate")
+    public Map  VipUpdate(@RequestParam Map map){
+
+        int i = vipService.VipUpdate(map);
+        Map temp=new HashMap();
+
+            temp.put("issuc",i);
+
+
+
+        return temp ;
+    }
+
+    /**
+     * 会员删除
+     * @param map
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/VipDelete")
+    public int VipDelete(@RequestParam Map map){
+        int i = vipService.VipDelete(map);
+        System.out.println(i);
+
+        return  i;
+    }
+
+}
